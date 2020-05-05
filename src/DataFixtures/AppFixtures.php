b1fcc2f688873fde1a2d0165231ddf97f868f252
lionel <sornom.lionel@gmail.com>
2020-05-05 09:29:13 +0200

    config user and test jwt

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,17 +6,25 @@ use App\Entity\Resume;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encodePassword;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoderInterface)
+    {
+        $this->encodePassword = $userPasswordEncoderInterface;
+    }
     public function load(ObjectManager $manager)
     {
+
         // $product = new Product();
         // $manager->persist($product);
         $user = new User();
         $user
             ->setEmail('test@test.fr')
-            ->setPassword('admin')
+            ->setPassword($this->encodePassword->encodePassword($user, "admin"))
             ->setRoles(["ROLE_ADMIN"])
             ;
         $manager->persist($user);
